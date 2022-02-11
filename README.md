94214e1bf8d04e7c7c841eaf908f54c5b66f3ae8
John Tannous <john.tannous97@gmail.com>
2022-02-11 14:23:56 -0800

    sending app to heroku

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Note-Taker
+# Project Name: Note-Taker
+https://github.com/johntannous/Note-Taker
+## Description
+This application is used to write and save notes. It uses Express.js back end and saves and retrieves note data from a JSON file.
+## Table of Contents
+  
+* [License](#license)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [Questions](#questions)
+  
+
+## License
+This project is licensed under the MIT license. 
+  
+## Installation
+  Please install these dependencies to run this application: Express.js
+  
+## Usage
+  To use this app, Please run node server.js in your terminal and type http://localhost:3001/ into your browser.
+  
+  Heroku deployed link:  
+  
+## Contribution
+  ​Contributors: John Tannous
+
+## Tests
+  The following is needed to run the test: node server.js
+
+## Questions
+  My GitHub profile link is: https://github.com/johntannous.
+  If you have any questions regarding this app, please contact directly at: john.tannous97@gmail.com.
+  
---
+++ server.js
@@ -0,0 +1,88 @@
+//Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+
+const allNotes = require("./Develop/db/db.json");
+
+//set up server
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Set up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Add a static middleware for serving assets in the public folder
+app.use(express.static("./Develop/public"));
+
+// Async processes
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// API Route | "GET" request
+app.get("/api/notes", function (req, res) {
+  readFileAsync("./Develop/db/db.json", "utf8").then(function (data) {
+    notes = [].concat(JSON.parse(data));
+    res.json(notes);
+  });
+});
+
+//API post requests
+app.post("/api/notes", function (req, res) {
+  const note = req.body;
+  readFileAsync("./Develop/db/db.json", "utf8")
+    .then(function (data) {
+      const notes = [].concat(JSON.parse(data));
+      note.id = notes.length + 1;
+      notes.push(note);
+      return notes;
+    })
+    .then(function (notes) {
+      writeFileAsync("./Develop/db/db.json", JSON.stringify(notes));
+      res.json(note);
+    });
+});
+
+// HTML Routes
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+//delete note
+function deleteNote(id, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+    let note = notesArray[i];
+    console.log(note);
+    if (note.id == id) {
+      notesArray.splice(i, 1);
+   
+      writeFileAsync("./Develop/db/db.json", JSON.stringify(notesArray));
+      break;
+    }
+  }
+  return notesArray;
+}
+
+app.delete("/api/notes/:id", (req, res) => {
+  readFileAsync("./Develop/db/db.json", "utf8").then(function (data) {
+    console.log(data);
+    notesArray = deleteNote(req.params.id, JSON.parse(data));
+    console.log(notesArray);
+    res.json(notesArray);
+  });
+});
+
+// Listen for connections
+app.listen(PORT, function () {
+  console.log("Example app listening at PORT " + PORT);
+});
